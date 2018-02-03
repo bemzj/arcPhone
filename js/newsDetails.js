01cc3891d2d4d2e815cea416f3160fe9c6a1e1bc
bemzj <qiuzijia520@qq.com>
2018-02-03 16:13:03 +0800

    xx

--- js/newsDetails.js
+++ js/newsDetails.js
@@ -1,20 +1,20 @@
 $(function(){
-	for(var i=0;i<$('#mainContent .content').find('img').length;i++)
-	{
-		if(i%2==0)
-		{
-			$('#mainContent .content').find('img').eq(i).attr('data-scroll-reveal','enter left and move 30px over 1.0s');
-		}else{
-			$('#mainContent .content').find('img').eq(i).attr('data-scroll-reveal','enter right and move 30px over 1.0s')
-		}
-		
-	}
-	//开启滑动动画
-	if(!(/msie [6|7|8|9]/i.test(navigator.userAgent))) {
-		(function() {
-			window.scrollReveal = new scrollReveal({
-				reset: true
-			});
-		})();
-	};
+//	for(var i=0;i<$('#mainContent .content').find('img').length;i++)
+//	{
+//		if(i%2==0)
+//		{
+//			$('#mainContent .content').find('img').eq(i).attr('data-scroll-reveal','enter left and move 30px over 1.0s');
+//		}else{
+//			$('#mainContent .content').find('img').eq(i).attr('data-scroll-reveal','enter right and move 30px over 1.0s')
+//		}
+//		
+//	}
+//	//开启滑动动画
+//	if(!(/msie [6|7|8|9]/i.test(navigator.userAgent))) {
+//		(function() {
+//			window.scrollReveal = new scrollReveal({
+//				reset: true
+//			});
+//		})();
+//	};
 });
