e0b22b25cf4d990915b45015235b6fd67d4dc827
Your Name <email@example.com>
2018-02-07 23:37:23 +0800

    xx

--- js/projects.js
+++ js/projects.js
@@ -13,22 +13,22 @@ $(function(){
 			},3000);
 		}
 	});
-	for(var i=0;i<$('.mainImgBox').length;i++)
-	{
-		if(i%2==0)
-		{
-			$('.mainImgBox').eq(i).attr('data-scroll-reveal','enter left and move 30px over 1.0s');
-		}else{
-			$('.mainImgBox').eq(i).attr('data-scroll-reveal','enter right and move 30px over 1.0s')
-		}
-		
-	}
-	//开启滑动动画
-	if(!(/msie [6|7|8|9]/i.test(navigator.userAgent))) {
-		(function() {
-			window.scrollReveal = new scrollReveal({
-				reset: true
-			});
-		})();
-	};
+//	for(var i=0;i<$('.mainImgBox').length;i++)
+//	{
+//		if(i%2==0)
+//		{
+//			$('.mainImgBox').eq(i).attr('data-scroll-reveal','enter left and move 30px over 1.0s');
+//		}else{
+//			$('.mainImgBox').eq(i).attr('data-scroll-reveal','enter right and move 30px over 1.0s')
+//		}
+//		
+//	}
+//	//开启滑动动画
+//	if(!(/msie [6|7|8|9]/i.test(navigator.userAgent))) {
+//		(function() {
+//			window.scrollReveal = new scrollReveal({
+//				reset: true
+//			});
+//		})();
+//	};
 });
